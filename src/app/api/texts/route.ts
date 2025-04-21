9cc7c578166212677ccb95097e93a78e91ea96b0
Eran Geva <eran.geva@gmail.com>
2025-04-21 13:50:59 +0100

    fix authOptions error

--- src/app/api/texts/route.ts
+++ src/app/api/texts/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getTexts, addText } from '@/db';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '../auth/[...nextauth]/auth';
 
 export async function GET() {
   try {
