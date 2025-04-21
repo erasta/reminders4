125a572247d1a49f63ef9269053c3fdb37d57b69
Eran Geva <eran.geva@gmail.com>
2025-04-21 12:34:20 +0100

    google login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LoginButton from "@/components/LoginButton";
 
 export default function Home() {
   return (
@@ -49,6 +50,10 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+
+        <div className="flex justify-center">
+          <LoginButton />
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
